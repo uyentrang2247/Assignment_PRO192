9e753dba86c1863ef095db83cc038d7a5be61a7a
uyentrang2247 <74484154+uyentrang2247@users.noreply.github.com>
2020-11-15 22:36:20 +0700

    Update Assignmnet_PRO192_TQT.java

--- Assignmnet_PRO192_TQT.java
+++ Assignmnet_PRO192_TQT.java
@@ -5,19 +5,24 @@
  */
 package assignmnet_pro192_tqt;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ADMIN
  */
-public class Assignmnet_PRO192_TQT {
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void showMenu(){
+public class Assignment_PRO192_TQT {
+
+    public static void showMenu() {
         System.out.println("--------------------------------");
         System.out.println("1.Add new tours to the list");
         System.out.println("2.List all domestic tours in the system");
@@ -33,9 +38,9 @@ public class Assignmnet_PRO192_TQT {
         System.out.println("12.Quit");
         System.out.println("----------------------------------");
         System.out.print("Please enter your choice: ");
-        
+
     }
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
@@ -44,42 +49,85 @@ public class Assignmnet_PRO192_TQT {
         do {
             showMenu();
             choice = sc.nextInt();
-            switch(choice){
-                case 1: 
+            switch (choice) {
+                case 1:
                     tourlist.addTour();
                     break;
                 case 2:
                     System.out.println("All domestic tours in the system: ");
-                    for (int i = 0; i <tourlist.DomesTourList().size(); i++)
+                    for (int i = 0; i < tourlist.DomesTourList().size(); i++) {
                         System.out.println(tourlist.DomesTourList().get(i));
+                    }
                     break;
                 case 3:
-                    System.out.print("Average tour charge of all international tours is: " + tourlist.averageInterToursCharge());
+                    System.out.println("Average tour charge of all international tours is: " + tourlist.averageInterToursCharge());
                     break;
                 case 4:
                     tourlist.searchTour();
                     break;
-                case 5: 
+                case 5:
+                    System.out.println("The first occurrence of the tour having minimum tour charge is:");
+                    System.out.println(tourlist.searchFirstOccurHavingMimTourCharge());
                     break;
                 case 6:
+                    System.out.print("Input tourCode to remove: ");
+                    String tourCode = sc.nextLine();
+                    if (tourlist.checkTourCode(tourCode)) {
+                        tourlist.removeTourCode(tourCode);
+                        System.out.println("Remove Success!");
+                    } else {
+                        System.out.println("tourCode doesn't exist.");
+                    }
                     break;
                 case 7:
+                    System.out.println("Input a date to remove all tours'start date after that: ");
+                    String date = sc.nextLine();
+                    if (tourlist.removeTourDate(date) > 0) {
+                        System.out.println("Remove Success " + tourlist.removeTourDate(date) + "tours.");
+                    } else {
+                        System.out.println("Remove 0 tour.");
+                    }
                     break;
                 case 8:
+                    tourlist.sortTour();
                     break;
                 case 9:
                     break;
-                case 10:
-                    break;
+                case 10: {
+                    FileOutputStream fos;
+                    ObjectOutputStream oos;
+                    try {
+                        fos = new FileOutputStream("Tourdata.dat");
+                        oos = new ObjectOutputStream(fos);
+                        oos.writeObject(tourlist.getList());
+                        oos.close();
+                        fos.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("File not found");
+                    } catch (IOException e) {
+                        System.out.println("Error initializing stream");
+                    }
+                }
+                break;
                 case 11:
+                    FileInputStream fis;
+                    ObjectInputStream ois;
+                    try {
+                        fis = new FileInputStream("Tourdata.dat");
+                        ois = new ObjectInputStream(fis);
+                        ArrayList<Tour> tour = (ArrayList<Tour>) ois.readObject();
+                        ois.close();
+                        fis.close();
+                    } catch (IOException | ClassNotFoundException ex) {
+                        Logger.getLogger(Assignment_PRO192_TQT.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
                     break;
-                case 12:
-                    break;
-                default : 
+                default:
                     System.out.println("Goodbye!");
                     break;
-            } 
-        }while (choice < 12 && choice > 0);
+            }
+        } while (choice < 12 && choice > 0);
     }
-    
+
 }
